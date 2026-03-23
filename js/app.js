f300f5ea9b86ec4e1609a880eb27375aa5d9d88a
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-03-23 10:57:50 +0530

    [Rithvik]Coded UC15 - to click

--- js/app.js
+++ js/app.js
@@ -16,3 +16,30 @@ document.addEventListener("DOMContentLoaded", async () => {
     populateDropdown(toSelect, units);
 
 });
+import { setActive } from "./ui.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+    const typeSelector = document.querySelector("#type-selector");
+    const fromSelect = document.querySelector("#from-unit");
+    const toSelect = document.querySelector("#to-unit");
+
+    const units = await getUnits(state.type);
+    populateDropdown(fromSelect, units);
+    populateDropdown(toSelect, units);
+
+    document.querySelectorAll(".type-card").forEach(card => {
+        card.addEventListener("click", async () => {
+
+            state.type = card.dataset.type;
+
+            setActive(typeSelector, card, ".type-card");
+
+            const units = await getUnits(state.type);
+
+            populateDropdown(fromSelect, units);
+            populateDropdown(toSelect, units);
+        });
+    });
+
+});
